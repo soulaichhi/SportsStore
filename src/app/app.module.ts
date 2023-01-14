0ff439f018e03a53fd354dd938555fe764d98e6c
soulaichhi <chhibou.soulaimane@gmail.com>
2023-01-14 17:36:47 +0100

    Complete the Store Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { StoreComponent } from './store/store.component';
+import {StoreModule} from "./store/store.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    StoreComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
