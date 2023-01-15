41d32a33e71b88b8f728cf40c3c9925ee920fb03
Soulaimane Chhibou <Chhibou.Soulaimane@gmail.com>
2023-01-15 23:23:43 +0100

    Complete Order and Checkout features

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from "@angular/core";
 import {ProductRepository} from "./product.repository";
 import {StaticDataSource} from "./static.datasource";
 import {Cart} from "./cart.model";
+import {Order} from "./order.model";
+import {OrderRepository} from "./order.repository";
 
 @NgModule({
   providers: [ProductRepository,
     StaticDataSource,
-    Cart]
+    Cart, Order, OrderRepository]
 })
 export class ModelModule{}
